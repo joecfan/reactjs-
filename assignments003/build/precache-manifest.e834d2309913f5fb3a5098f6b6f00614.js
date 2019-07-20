158eb76856e2dbf91524f4c6b9437632fa232883
joecfan <baozhuohui@126.com>
2019-07-20 22:04:29 +0800

    Add files via upload

---
+++ assignments003/build/precache-manifest.e834d2309913f5fb3a5098f6b6f00614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381100c9907a93620e1277f283fb44b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0272377c770a5864d22",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2d75c3129faaaa270435",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f0272377c770a5864d22",
+    "url": "/static/js/2.76335c94.chunk.js"
+  },
+  {
+    "revision": "2d75c3129faaaa270435",
+    "url": "/static/js/main.70e79ca9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ assignments003/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import logo from './logo.svg';
+import './App.css';
+import VideoPlayer from './VideoPlayer';
+
+function App() {
+  
+  return (
+    <VideoPlayer />
+  );
+}
+
+export default App;
